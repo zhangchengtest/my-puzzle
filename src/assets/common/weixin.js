29a6a4a49eabb2332cb96f7eea732b50bd824dd4
zhangcheng <52xinyun>
2023-04-05 00:28:20 +0800

    update

--- src/assets/common/weixin.js
+++ src/assets/common/weixin.js
@@ -45,6 +45,8 @@ export const wexinShare = (data, shareData) => {
             }
         })
 
+        wx.showOptionMenu();
+
     });
     wx.error(function(res) {
         // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
