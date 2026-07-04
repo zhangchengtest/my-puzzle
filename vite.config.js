abfb25b80e2d441ed400e8695bb246efa1716574
676931854@qq.com <676931854@qq.com>
2026-07-04 14:20:11 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
     base: '/mix', // 设置publicPath
   server: {
     proxy: {
-      '/downloads': {
+      '/files': {
         target: 'https://clock.cuiyi.club',
         changeOrigin: true,
       },
