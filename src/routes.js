3862c17e8cb96bd75132a08799cefd9d3099542f
676931854@qq.com <676931854@qq.com>
2026-07-29 15:37:29 +0800

    update

--- src/routes.js
+++ src/routes.js
@@ -23,6 +23,7 @@ const routes = [
       DaLiuRen: '大六壬排盘 - Elephant',
       Downloads: '文件管理 - Elephant',
       Career: '简历与面试 - Elephant',
+      Chat: '房间聊天 - Elephant',
       // 你可以继续补充更多组件名与标题的映射
     };
 
